f2f9b4d178a72f278f26643008677da9767f5831
baianausupbaeva@gmail.com <baianausupbaeva@gmail.com>
2025-01-25 00:17:24 +0100

    create register function

--- app/services/api.ts
+++ app/services/api.ts
@@ -13,14 +13,24 @@ export const api = axios.create({
 
 // Typ för svaret från inloggnings-API:t
 interface LoginResponse {
-  userId: number; // Om backend returnerar användarens ID
+  id: number; // Om backend returnerar användarens ID
   token?: string; // Om backend skickar en token (JWT eller liknande)
 }
 
 // Funktion för att hantera inloggningsanrop
 export const login = async (username: string, password: string): Promise<LoginResponse> => {
   // Använd Axios generiska typning för att specificera LoginResponse
-  const response = await api.post<LoginResponse>('/auth/login', { username, password });
-
+  const response = await api.post<LoginResponse>('/user/login', { username, password });
+  console.log(response)
   return response.data; // Returnera typat svar från backend
+};
+
+
+// Function to handle registration (API call)
+export const register = async (username: string, password: string): Promise<void> => {
+  const response = await api.post("/user", {
+    username,
+    password,
+  });
+  return response.data; // Return backend response (can be a message or token)
 };
